36887cf4762dfc074265a1fa02451df9b9e0cd37
zoltanvb <balogh.zoltan.v@gmail.com>
2025-02-13 06:57:53 +0100

    Script update for repos with "main" branch instead of "master"

--- update_modules.sh
+++ update_modules.sh
@@ -7,7 +7,14 @@ do
 	echo "$d"
     echo ----
     cd "$thumbdir"/"$d"
-    git checkout master
-    git pull origin master --depth=20
+    git rev-parse --verify main 2>/dev/null
+    retVal=$?
+    if [ $retVal -ne 0 ]; then
+        git checkout master
+        git pull origin master --depth=20
+    else
+        git checkout main
+        git pull origin main --depth=20
+    fi
     [[ $1 = "gc" ]] && git gc --prune=all
 done
